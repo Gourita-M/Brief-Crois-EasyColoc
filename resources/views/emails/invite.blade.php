a3820d24934d2e6c74bf6b28050a3e674e7b9a22
Mouad Gourita <mouad.gourita@gmail.com>
2026-02-27 11:42:28 +0000

    feat : Invitation View

---
+++ resources/views/emails/invite.blade.php
@@ -0,0 +1,8 @@
+<h2>You’re Invited 🎉</h2>
+
+<p>You have been invited to join a shared accommodation.</p>
+
+<a href="{{$token}}"
+   style="display:inline-block;padding:10px 20px;background:#4f46e5;color:white;text-decoration:none;border-radius:6px;">
+   Join Accommodation
+</a>
