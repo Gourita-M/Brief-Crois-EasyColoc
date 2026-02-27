fd774adff3cacfe972fbc35d19668212d72897a0
Mouad Gourita <mouad.gourita@gmail.com>
2026-02-27 11:40:38 +0000

    feat : Query builder to show who owns who

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,6 +9,7 @@ use App\Models\Accommodations;
 use App\Models\Persons;
 use App\Models\Memberships;
 use App\Models\Expenses;
+use App\Models\Payments;
 
 class DashboardController extends Controller
 {
@@ -19,6 +20,7 @@ class DashboardController extends Controller
         $accommodation = Accommodations::where('persons_id', $person->id)
                                         ->where('status', 'Active')->first();
         if($accommodation){
+
         $membershipscount = Memberships::where('accommodations_id', $accommodation->id)
                                         ->count();
 
@@ -29,7 +31,42 @@ class DashboardController extends Controller
             ->where('e.accommodations_id', $accommodation->id)
             ->get();
 
-            return View('owner.dashboard', Compact('accommodation','membershipscount', 'expenses'));
+        $sum = DB::table('payments as p')
+            ->join('expenses as e', 'p.expenses_id', '=', 'e.id')
+            ->join('persons as member', 'p.persons_id', '=', 'member.id')
+            ->join('users as member_user', 'member.users_id', '=', 'member_user.id')
+            ->join('persons as creator', 'e.persons_id', '=', 'creator.id')
+            ->join('users as creator_user', 'creator.users_id', '=', 'creator_user.id')
+            ->where('p.status', 0)
+            ->where('e.accommodations_id', $accommodation->id)
+            ->select(
+                'p.id as payment_id',
+                'member_user.name as member_name',
+                'e.title as expense_title',
+                'creator_user.name as expense_creator',
+                'p.amount as total_owed'
+            )
+            ->get();
+             $membership = Memberships::where('persons_id', $person->id)
+                                ->where('role', 'Owner')->first();
+       
+      
+
+            $members = DB::table('memberships as m')
+            ->leftJoin('persons as p', 'm.persons_id', '=', 'p.id')
+            ->leftJoin('users as u', 'p.users_id', '=', 'u.id')
+            ->where('m.accommodations_id', $membership->accommodations_id)
+            ->select(
+                'm.id as membership_id',
+                'm.persons_id',
+                'p.users_id as person_users_id',
+                'u.id as user_id',
+                'u.name'
+            )
+            ->get();
+            
+        
+            return View('owner.dashboard', Compact('accommodation','membershipscount', 'expenses', 'sum' ,'members'));
         
         }else{
 
@@ -72,7 +109,24 @@ class DashboardController extends Controller
             ->where('e.accommodations_id', $accommodationinfo->id)
             ->get();
 
-        return View('owner.userdashboard', Compact('accommodationinfo', 'members', 'expenses'));
+        $sum = DB::table('payments as p')
+            ->join('expenses as e', 'p.expenses_id', '=', 'e.id')
+            ->join('persons as member', 'p.persons_id', '=', 'member.id')
+            ->join('users as member_user', 'member.users_id', '=', 'member_user.id')
+            ->join('persons as creator', 'e.persons_id', '=', 'creator.id')
+            ->join('users as creator_user', 'creator.users_id', '=', 'creator_user.id')
+            ->where('p.status', 0)
+            ->where('e.accommodations_id', $accommodationinfo->id)
+            ->select(
+                'p.id as payment_id',
+                'member_user.name as member_name',
+                'e.title as expense_title',
+                'creator_user.name as expense_creator',
+                'p.amount as total_owed'
+            )
+            ->get();
+
+        return View('owner.userdashboard', Compact('accommodationinfo', 'members', 'expenses' , 'sum'));
         
         }else{
             $expenses = null;
